9aa44ce5ace31e7f5768b8b9d46af8d94fe4678d
DevikaKhosla <dkhosla_9@hotmail.com>
2017-02-04 16:34:35 -0500

    Update testfile.py

--- testfile.py
+++ testfile.py
@@ -1,1 +1,20 @@
 
+#key statistics from yahoo api
+
+import urllib2
+import json
+
+#function to be used in another script
+
+def get_key_stats_api(stock):
+
+	api_url = "https://query2.finance.yahoo.com/v10/finance/quoteSummary/%s?modules=defaultKeyStatistics" % stock
+
+	response = urllib2.urlopen(api_url)
+	key_statistics_json = response.read()
+
+	key_statistics_dict = json.loads(key_statistics_json)
+	
+	return key_statistics_dict
+
+print get_key_stats_api("MSFT")
